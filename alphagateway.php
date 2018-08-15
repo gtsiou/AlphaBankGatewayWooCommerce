c4be3dbc7160658f835eb5dac3d34a05527e9374
gtsiou <gtsiou@outlook.com>
2018-08-15 13:47:12 +0200

    Added the PR of masterpass with proper formatting

--- alphagateway.php
+++ alphagateway.php
@@ -1,33 +1,40 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit; // Exit if accessed directly
+if (! defined('ABSPATH')) {
+    exit(); // Exit if accessed directly
 }
 
 /*
-Plugin Name: Alpha Bank gateway - WooCommerce Gateway
-Plugin URI: https://github.com/Antreasgr/AlphaBankGatewayWooCommerce
-Description: Extends WooCommerce by Adding custom gateway
-Version: 1.0
-Author: Antreas Gribas
-Author URI: https://github.com/Antreasgr/
-*/
+ * Plugin Name: Alpha Bank gateway - WooCommerce Gateway
+ * Plugin URI: https://github.com/Antreasgr/AlphaBankGatewayWooCommerce
+ * Description: Extends WooCommerce by Adding custom gateway
+ * Version: 1.0
+ * Author: Antreas Gribas
+ * Author URI: https://github.com/Antreasgr/
+ */
 
 // Include our Gateway Class and register Payment Gateway with WooCommerce
-add_action( 'plugins_loaded', 'gateway_init', 0 );
-function gateway_init() {
-	// If the parent WC_Payment_Gateway class doesn't exist
-	// it means WooCommerce is not installed on the site
-	// so do nothing
-	if ( ! class_exists( 'WC_Payment_Gateway' ) ) return;
-	
-	// If we made it this far, then include our Gateway Class
-	include_once( 'alphagateway_class.php' );
+add_action('plugins_loaded', 'gateway_init', 0);
 
-	// Now that we have successfully included our class,
-	// Lets add it too WooCommerce
-	add_filter( 'woocommerce_payment_gateways', 'add_mygateway' );
-	function add_mygateway( $methods ) {
-		$methods[] = 'WC_Gateway_Alpha';
-		return $methods;
-	}
+function gateway_init()
+{
+    // If the parent WC_Payment_Gateway class doesn't exist
+    // it means WooCommerce is not installed on the site
+    // so do nothing
+    if (! class_exists('WC_Payment_Gateway'))
+        return;
+
+    // If we made it this far, then include our Gateway Class
+    include_once ('alphagateway_class.php');
+    include_once ('alphagatewaymasterpass_class.php');
+
+    // Now that we have successfully included our class,
+    // Lets add it too WooCommerce
+    add_filter('woocommerce_payment_gateways', 'add_mygateway');
+
+    function add_mygateway($methods)
+    {
+        $methods[] = 'WC_Gateway_Alpha';
+        $methods[] = 'WC_Gateway_Alpha_Masterpass';
+        return $methods;
+    }
 }
